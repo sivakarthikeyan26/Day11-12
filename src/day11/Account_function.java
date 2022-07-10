2d9b6663513dad5fe7e58842d1fa5027e9da9361
sivakarthikeyan26 <50573680+sivakarthikeyan26@users.noreply.github.com>
2022-07-10 21:22:56 +0530

    Program to simulate money withdrawal is implemented.

--- src/day11/Account_function.java
+++ src/day11/Account_function.java
@@ -2,11 +2,16 @@ package day11;
 
 public class Account_function implements Account_interface {
 	private double total_balance;
-
+	/*
+	 * Initalization of the total balance.
+	 */
 	public Account_function() {
 		this.total_balance = 200000;
 	}
-
+	/**
+	 * Method to withdraw the amount from total balance.
+	 * @param amnt - passing the amount to withdraw from the account.
+	 */
 	public void compute(double amnt) {
 		if (amnt > total_balance) {
 			System.out.println("Sorry you money cannot be debited as it exceeds the total available balance");
@@ -17,11 +22,15 @@ public class Account_function implements Account_interface {
 		}
 
 	}
-
+	/*
+	 * Method to display the available balance,
+	 */
 	public void display() {
 		System.out.println("Your available balance is = " + total_balance);
 	}
-
+	/*
+	 * Method to ask the users whether they want to continue withdrawal process.
+	 */
 	public boolean message() {
 		System.out.println("Do yo wish to continue Y/N");
 		char ch = Account.sc.next().charAt(0);
